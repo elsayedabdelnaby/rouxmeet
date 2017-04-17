5665f940f926501f2d73e515e81a144eddd2f7e0
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-04-17 11:56:51 +0200

    handle speakers list and finish my rouxmeet website

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,52 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   var myArtwork = [];
+  var myArtists = [];
+
+  myArtists = appdata.speakers;
   appdata.speakers.forEach(function (item) {
     myArtwork = myArtwork.concat(item.artwork);
   });
   res.render('index', {
     title: 'Home',
-    artwork: myArtwork
+    artwork: myArtwork,
+    artists: myArtists,
+    page: 'home'
+  });
+});
+
+/* GET speakers page. */
+router.get('/speakers', function (req, res, next) {
+  var myArtwork = [];
+  var myArtists = [];
+  myArtists = appdata.speakers;
+
+  appdata.speakers.forEach(function (item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork,
+    artists: myArtists,
+    page: 'artistList'
+  });
+});
+/* GET speaker page. */
+router.get('/speakers/:speakerid', function (req, res, next) {
+  var myArtwork = [];
+  var myArtists = [];
+
+  appdata.speakers.forEach(function (item) {
+    if (item.shortname == req.params.speakerid) {
+      myArtists.push(item);
+      myArtwork = myArtwork.concat(item.artwork);
+    }
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork,
+    artists: myArtists,
+    page: 'artistDetail'
   });
 });
 
