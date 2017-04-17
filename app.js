e46391b75167591b084fd507cd9f15985ded7115
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-04-17 10:54:44 +0200

    importing our data and fetch nextmeeting

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.appdata = require('./data.json');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
